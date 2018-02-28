a3a2243fa47be83416efd77a456038489970b183
ongfei <ong_fei@163.com>
2018-02-28 15:38:53 +0800

    a

---
+++ BaseTool/BaseTool/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaseTool
+//
+//  Created by ongfei on 2018/2/28.
+//  Copyright © 2018年 ongfei. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
